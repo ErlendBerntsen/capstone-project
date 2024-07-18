52d8193cc370ff93a907bc9b7eb0652255920ae0
Erlend Berntsen <erlend.berntsen@capgemini.com>
2024-07-18 10:02:58 +0200

    added html tags and meta tags

---
+++ src/Footer.js
@@ -0,0 +1,35 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <img src="./logo.png" alt="logo"></img>
+            <div>
+                <h3>Doormat Navigation</h3>
+                <ul>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="/home">About</a></li>
+                    <li><a href="/home">Menu</a></li>
+                    <li><a href="/home">Reservations</a></li>
+                    <li><a href="/home">Order Online</a></li>
+                    <li><a href="/home">Log In</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>Contact</h3>
+                <ul>
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3>Social Media Links</h3>
+                <ul>
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import { Nav } from './Nav';
+
+
+export const Header = () => {
+    return (
+        <header>
+            <img src="./Logo.svg" alt="logo"></img>
+            <Nav></Nav>
+        </header>
+    )
+}
