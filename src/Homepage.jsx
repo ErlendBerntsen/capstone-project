fb3449b923dae5ea59567f4f0b715c65ac1a4dfc
Erlend Berntsen <erlend.berntsen@capgemini.com>
2024-10-06 15:31:57 +0200

    fixed margin and and started on specials component

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,9 +1,11 @@
 import { Hero } from "./Hero";
+import { Specials } from "./Specials";
 
 export const HomePage = () => {
     return (
-        <>
+        <div className="homepage-container">
             <Hero></Hero>
-        </>
+            <Specials></Specials>
+        </div>
     );
 }
---
+++ src/Specials.js
@@ -0,0 +1,11 @@
+export const Specials = () => {
+    return (
+        <div className="specials-container">
+            <div className="specials-header">
+                <h2>This weeks specials!</h2>
+                <a href="/" className="specials-menu-button">Online menu</a>
+            </div>
+            <p>asdasd</p>
+        </div>
+    );
+}
