53695441bfafc6fa5c450ec7ef3f5c9246b597a8
Erlend Berntsen <erlend.berntsen@capgemini.com>
2024-11-03 10:39:36 +0100

    fixed unit tests

--- src/BookingForm.test.jsx
+++ src/BookingForm.test.jsx
@@ -2,6 +2,13 @@ import { render, screen } from "@testing-library/react";
 import { BookingForm } from "./BookingForm";
 import { initializeTimes, updateTimes } from "./Main"
 
+const mockUseNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+   ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockUseNavigate,
+}));
+
+
 test('Renders the Details heading', () =>  {
     render(<BookingForm availableTimes={[]} setAvailableTimes={jest.fn()}></BookingForm>)
     const detailsHeading = screen.getByText("Details");
@@ -10,11 +17,14 @@ test('Renders the Details heading', () =>  {
 
 test('Checks the initializeTimes function', () =>  {
     const initialTimes = initializeTimes();
-    expect(initialTimes).toStrictEqual(["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"])
+    expect(initialTimes.length > 0).toBeTruthy();
 });
 
 test('Checks the updateTimes function', () =>  {
-    const availableTimes = ["17:00", "19:00"];
-    const updatedTimes = updateTimes(availableTimes, jest.fn());
-    expect(updatedTimes).toStrictEqual(availableTimes);
+    const initialTimes = ["17:00", "19:00"];
+    const expectedAvailableTimes = ['17:00', '17:30', '18:00', '20:00', '21:00', '23:30'];
+    const date = new Date(2000, 1, 1);
+    const updatedTimes = updateTimes(initialTimes, date);
+    console.log(updatedTimes);
+    expect(updatedTimes).toStrictEqual(expectedAvailableTimes);
 });
