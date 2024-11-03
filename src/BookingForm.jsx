2c94a2d59eb02127bfb6eb760f40eb25e966f4ea
Erlend Berntsen <erlend.berntsen@capgemini.com>
2024-11-03 12:30:19 +0100

    added form validation

--- src/BookingForm.jsx
+++ src/BookingForm.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const BookingForm = ({availableTimes, setAvailableTimes, submitForm}) => {
     const [formData, setFormData] = useState({
-        date: new Date(),
-        time: "17:00",
+        date: "",
+        time: "",
         guests: "1",
         occasion: "None",
         seating: "inside",
@@ -17,13 +17,25 @@ export const BookingForm = ({availableTimes, setAvailableTimes, submitForm}) => 
 
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const copy = {...formData};
+        if(copy.date !== ""){
+            copy.time = availableTimes[0];
+            setFormData(copy);
+        }
+    }, [availableTimes]);
+
     const handleFormChange = (event, input) => {
         const copy = {...formData};
         copy[input] = event.target.value;
         if(input === "date"){
+            if(event.target.value === ""){
+                copy.time = "";
+            }
             setAvailableTimes(copy.date)
         }
         setFormData(copy);
+        console.log(copy);
     }
 
     const handleSubmit = (event) => {
@@ -32,26 +44,34 @@ export const BookingForm = ({availableTimes, setAvailableTimes, submitForm}) => 
         };
     }
 
+    const timeOptions = () => {
+        if(formData.date === ""){
+            return ["Select date first"];
+        }else{
+            return availableTimes;
+        }
+    }
+
     return (
-        <form className="bookingpage-form" onSubmit={handleSubmit}>
+        <form id="bookingpage-form" className="bookingpage-form" onSubmit={handleSubmit}>
             <div className="bookingpage-details">
                 <h2 className="bookingpage-form-title">Details</h2>
                 <div className="bookingpage-form-row">
                     <div className="bookingpage-form-col">
-                        <label htmlFor="reservation-date" className="bookingpage-form-label">Choose date</label>
-                        <input input type="date" id="reservation-date" onChange={(e) => handleFormChange(e, "date")} className="bookingpage-form-date-input"></input>
+                        <label htmlFor="reservation-date" className="bookingpage-form-label">Choose date *</label>
+                        <input input type="date" id="reservation-date" onChange={(e) => handleFormChange(e, "date")} className="bookingpage-form-date-input" required min={new Date().toISOString().substring(0, 10)}></input>
                     </div>
                     <div className="bookingpage-form-col">
-                        <label htmlFor="reservation-time" className="bookingpage-form-label">Choose time</label>
-                        <select id="reservation-time" onChange={(e) => handleFormChange(e, "time")} className="bookingpage-form-select">
-                            {availableTimes.map(x => <option key={x}>{x}</option>)}
+                        <label htmlFor="reservation-time" className="bookingpage-form-label">Choose time *</label>
+                        <select id="reservation-time" onChange={(e) => handleFormChange(e, "time")} className="bookingpage-form-select" required>
+                            {timeOptions().map(x => <option key={x}>{x}</option>)}
                     </select>
                     </div>
                 </div>
                 <div className="bookingpage-form-row">
                     <div className="bookingpage-form-col">
-                        <label htmlFor="guests" className="bookingpage-form-label">Number of guests</label>
-                        <input type="number" id="guests" placeholder="1" min="1" max="10" onChange={(e) => handleFormChange(e, "guests")} className="bookingpage-form-text-input"></input>
+                        <label htmlFor="guests" className="bookingpage-form-label">Number of guests *</label>
+                        <input type="number" id="guests" value={formData.guests} min="1" max="10" onChange={(e) => handleFormChange(e, "guests")} className="bookingpage-form-text-input" required></input>
                     </div>
                     <div className="bookingpage-form-col">
                         <label htmlFor="occasion" className="bookingpage-form-label">Occasion</label>
@@ -81,21 +101,21 @@ export const BookingForm = ({availableTimes, setAvailableTimes, submitForm}) => 
                 <div className="bookingpage-form-row">
                     <div className="bookingpage-form-col">
                         <label htmlFor="firstname" className="bookingpage-form-label">First name *</label>
-                        <input type="text" id="firstname" className="bookingpage-form-text-input" onChange={(e) => handleFormChange(e, "firstName")}></input>
+                        <input type="text" id="firstname" className="bookingpage-form-text-input" onChange={(e) => handleFormChange(e, "firstName")} required></input>
                     </div>
                     <div className="bookingpage-form-col">
                         <label htmlFor="lastname" className="bookingpage-form-label">Last name *</label>
-                        <input type="text" id="lastname" className="bookingpage-form-text-input" onChange={(e) => handleFormChange(e, "lastName")}></input>
+                        <input type="text" id="lastname" className="bookingpage-form-text-input" onChange={(e) => handleFormChange(e, "lastName")} required></input>
                     </div>
                 </div>
                 <div className="bookingpage-form-row">
                     <div className="bookingpage-form-col">
                         <label htmlFor="phonenumber" className="bookingpage-form-label">Phone number *</label>
-                        <input type="tel" id="phonenumber" className="bookingpage-form-text-input" onChange={(e) => handleFormChange(e, "phoneNumber")}></input>
+                        <input type="tel" id="phonenumber" className="bookingpage-form-text-input" onChange={(e) => handleFormChange(e, "phoneNumber")} required></input>
                     </div>
                     <div className="bookingpage-form-col">
                         <label htmlFor="email" className="bookingpage-form-label">Email *</label>
-                        <input type="email" id="email" className="bookingpage-form-text-input" onChange={(e) => handleFormChange(e, "email")}></input>
+                        <input type="email" id="email" className="bookingpage-form-text-input" onChange={(e) => handleFormChange(e, "email")} required></input>
                     </div>
                 </div>
                 <div className="bookingpage-form-row">
@@ -105,7 +125,11 @@ export const BookingForm = ({availableTimes, setAvailableTimes, submitForm}) => 
                     </div>
                 </div>
             </div>
-            <input type="submit" className="bookingpage-form-button" value="Make your reservation"></input>
+            <input type="submit" className="bookingpage-form-button" value="Make your reservation" disabled={!isTimeValid(formData.time)}></input>
         </form>
     );
+}
+
+export const isTimeValid = (time) => {
+    return time !== "Select date first" && time !== "";
 }
--- src/BookingForm.test.jsx
+++ src/BookingForm.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { BookingForm } from "./BookingForm";
+import { BookingForm, isTimeValid } from "./BookingForm";
 import { initializeTimes, updateTimes } from "./Main"
 
 const mockUseNavigate = jest.fn();
@@ -25,6 +25,51 @@ test('Checks the updateTimes function', () =>  {
     const expectedAvailableTimes = ['17:00', '17:30', '18:00', '20:00', '21:00', '23:30'];
     const date = new Date(2000, 1, 1);
     const updatedTimes = updateTimes(initialTimes, date);
-    console.log(updatedTimes);
     expect(updatedTimes).toStrictEqual(expectedAvailableTimes);
+});
+
+test('Check the HTML5 validation is correct', () =>  {
+    render(<BookingForm availableTimes={[]} setAvailableTimes={jest.fn()}></BookingForm>)
+
+    const date = screen.getByLabelText("Choose date *");
+    expect(date).toHaveAttribute("required");
+    expect(date.getAttribute("type")).toBe("date");
+    expect(date.getAttribute("min")).toBe(new Date().toISOString().substring(0, 10));
+
+    const time = screen.getByLabelText("Choose time *");
+    expect(time).toHaveAttribute("required");
+
+    const guests = screen.getByLabelText("Number of guests *");
+    expect(guests).toHaveAttribute("required");
+    expect(guests.getAttribute("type")).toBe("number");
+    expect(guests.getAttribute("min")).toBe("1");
+    expect(guests.getAttribute("max")).toBe("10");
+
+    const firstName = screen.getByLabelText("First name *");
+    expect(firstName).toHaveAttribute("required");
+
+    const lastName = screen.getByLabelText("Last name *");
+    expect(lastName).toHaveAttribute("required");
+
+    const phoneNumber = screen.getByLabelText("Phone number *");
+    expect(phoneNumber).toHaveAttribute("required");
+    expect(phoneNumber.getAttribute("type")).toBe("tel");
+
+    const email = screen.getByLabelText("Email *");
+    expect(email).toHaveAttribute("required");
+    expect(email.getAttribute("type")).toBe("email");
+});
+
+test('Checks the invalid states of the time form input', () =>  {
+    const invalidTime1 = "";
+    const invalidTime2 = "Select date first";
+    expect(isTimeValid(invalidTime1)).toBe(false);
+    expect(isTimeValid(invalidTime2)).toBe(false);
+});
+
+test('Checks some valid states of the time form input', () =>  {
+    const validTime1 = "17:00";
+    const validTime2 = "20:30";
+    expect(isTimeValid(validTime1)).toBe(true);
+    expect(isTimeValid(validTime2)).toBe(true);
 });
